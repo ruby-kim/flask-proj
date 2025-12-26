f62b64f32919064b0d930e532b31516d1af63ebd
rubykim-sibel <ruby.kim@sibelhealth.com>
2025-12-26 19:14:25 +0900

    feat: update config

--- app/config.py
+++ app/config.py
@@ -7,10 +7,15 @@ class Config:
     TURSO_DATABASE_URL = os.getenv("TURSO_DATABASE_URL")
     TURSO_AUTH_TOKEN = os.getenv("TURSO_AUTH_TOKEN")
 
-    if not TURSO_DATABASE_URL and not TURSO_AUTH_TOKEN:
+    print(TURSO_AUTH_TOKEN)
+    print(TURSO_DATABASE_URL)
+
+    if TURSO_DATABASE_URL and TURSO_AUTH_TOKEN:
         # .env에 값이 있을 시에만 Turso를 사용하도록 설정
         SQLALCHEMY_DATABASE_URI = f"sqlite+{TURSO_DATABASE_URL}?secure=true"
         CONNECT_ARGS = {"auth_token": TURSO_AUTH_TOKEN}
+
+        print("클라우드 연결")
     else:
         # Turso 정보가 없거나 에러가 발생한다면 로컬 db를 사용하도록 설정
         print("\n[INFO] No Turso Setup: Use local db...\n")
